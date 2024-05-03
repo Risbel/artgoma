3be4e70d3e3d86c16ed19b3fb0e6fe3451e78587
Risbel Suarez <risbel961019@gmail.com>
2024-05-03 10:56:16 -0400

    fix: Fix query to get visits instead subscriptions

--- src/api/getConfirmations.ts
+++ src/api/getConfirmations.ts
@@ -1,13 +1,10 @@
 export const getConfirmations = async () => {
-  const res = await fetch(
-    `https://artworld-api.myaipeople.com/api/subscriptions_by_event/ba688a61-71bb-4677-b180-8d40f3c82796/`,
-    {
-      next: {
-        tags: ["subscriptions"],
-        revalidate: 3600,
-      },
-    }
-  );
+  const res = await fetch(`https://artworld-api.myaipeople.com/api/visits/`, {
+    next: {
+      tags: ["subscriptions"],
+      revalidate: 3600,
+    },
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -19,32 +16,28 @@ export const getConfirmations = async () => {
 };
 
 export interface IConfirmations {
-  count: 1;
-  next: null;
-  previous: null;
+  count: number;
+  next: string;
+  previous: string;
   results: [
     {
       id: string;
-      first_name: string;
-      last_name: string;
-      phone: string;
+      name: string;
+      slug: string;
+      code: string;
+      slogan: string;
+      image: string;
+      logo: string;
       email: string;
+      phone: string;
       address: string;
-      company: string;
       is_active: boolean;
       created_at: string;
       updated_at: string;
-      companions: [
-        {
-          id: string;
-          first_name: string;
-          last_name: string;
-          subscription: string;
-          is_active: boolean;
-          created_at: string;
-          updated_at: string;
-        }
-      ];
+      image_heigth: number;
+      logo_heigth: number;
+      image_width: number;
+      logo_width: number;
     }
   ];
 }
