cde3541b4daaa2d3089532bb473877bf6082db2b
Risbel Suarez <risbel961019@gmail.com>
2024-05-05 09:03:31 -0400

    style: Restyle the welcome dinamic route

--- src/app/[id]/components/H1Text.tsx
+++ src/app/[id]/components/H1Text.tsx
@@ -1,10 +1,10 @@
 const H1Text = () => {
   return (
-    <h1 className="text-center text-6xl font-semibold text-white">
+    <h1 className="text-center text-6xl font-semibold">
       <span className="pb-4 inline-flex animate-background-shine bg-[linear-gradient(110deg,#f7ff62,45%,#ffffff,55%,#f7ff62)] bg-[length:250%_100%] bg-clip-text text-transparent font-extrabold">
         VIP ENTRY
       </span>{" "}
-      <br /> <span className="animate-pulse">FREE ACCESS</span>
+      <br /> <span className=" text-white text-shadow shadow-slate-500">FREE ACCESS</span>
     </h1>
   );
 };
--- src/app/[id]/components/H2Text.tsx
+++ src/app/[id]/components/H2Text.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const H2Text = () => {
   return (
     <h2 className="text-3xl">
-      {" "}
-      <span className="shadow-red-950 text-shadow inline-flex animate-background-shine bg-[linear-gradient(110deg,#aa1212,45%,#f37272,55%,#aa1212)] bg-[length:250%_100%] bg-clip-text text-transparent font-semibold">
+      <span className="inline-flex animate-background-shine bg-[linear-gradient(110deg,#aa1212,45%,#f37272,55%,#aa1212)] bg-[length:250%_100%] bg-clip-text text-transparent font-semibold">
         ENJOY LIVE PAINTING
       </span>
     </h2>
