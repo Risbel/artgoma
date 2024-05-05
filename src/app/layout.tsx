a6364fb734b8fd6a13189e7aada724079dfa786d
Risbel Suarez <risbel961019@gmail.com>
2024-05-05 09:23:10 -0400

    fix: Remove provider import in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import CollaboratorProvider from "@/providers/CollaboratorProvider";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
