50f1cda88d7653b0ebd8f3f33ffbdb7b2655c867
Risbel Suarez <risbel961019@gmail.com>
2024-05-06 11:47:34 -0400

    feat: Add mobile view on `GetInTouch`

--- src/app/components/sections/getInTouch/GetInTouch.tsx
+++ src/app/components/sections/getInTouch/GetInTouch.tsx
@@ -5,13 +5,19 @@ import { Mail, PhoneCall, User2Icon, UserCircle2 } from "lucide-react";
 const GetInTouch = () => {
   return (
     <div className="bg-[#1c1f24] pb-16">
-      <div className="flex justify-end px-32">
+      <div className="flex justify-end px-10 lg:px-32">
         <H2GetInTouch />
       </div>
-      <div className="flex justify-between pl-16 pr-32 pt-16">
-        <Image width={400} height={100} src={"/Logo Goma horizontal.svg"} alt="Logo Goma horizontal" />
-        <div className="flex flex-col">
-          <div className="flex items-center border border-red-600 rounded-3xl overflow-hidden mb-4">
+      <div className="flex flex-col md:flex-row items-end justify-between pl-16 pr-10 md:pr-32 pt-6 md:pt-16">
+        <Image
+          className="hidden md:block"
+          width={400}
+          height={100}
+          src={"/Logo Goma horizontal.svg"}
+          alt="Logo Goma horizontal"
+        />
+        <div className="flex flex-col w-4/5 md:w-1/5">
+          <div className="flex items-center justify-between border border-red-600 rounded-3xl overflow-hidden mb-4">
             <p className="text-white px-4 py-2">CONTACT US</p>
             <div className="bg-red-600 px-4 py-2">
               <User2Icon fill="#ffffff" stroke="none" height={40} width={40} />
--- src/app/components/sections/getInTouch/H2GetInTouch.tsx
+++ src/app/components/sections/getInTouch/H2GetInTouch.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const H2GetInTouch = () => {
   return (
-    <h2 className="text-white text-6xl font-semibold">
+    <h2 className="text-white text-3xl md:text-4xl lg:text-6xl font-semibold text-end">
       LET'S GET IN <span className="text-red-600">TOUCH</span>
     </h2>
   );
