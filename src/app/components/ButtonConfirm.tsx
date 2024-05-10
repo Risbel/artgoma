ec419c1079ef6c85185c30f3596f5a917021da9a
Risbel Suarez <risbel961019@gmail.com>
2024-05-09 23:58:03 -0400

    style: Add style on call to action

--- src/app/components/ButtonConfirm.tsx
+++ src/app/components/ButtonConfirm.tsx
@@ -3,11 +3,13 @@ import React from "react";
 
 const ButtonConfirm = () => {
   return (
-    <Link
-      href={"/confirm"}
-      className="hover:-translate-y-1 transition-transform duration-300 bg-black/40 backdrop-blur-sm px-4 md:px-8 py-2 md:py-2 border-2 border-red-700 rounded-3xl"
-    >
-      <span className="text-white tracking-wider">Confirm</span>
+    <Link href={"/confirm"} className="">
+      <div className="shadow-xl hover:shadow-red-950 transition-shadow duration-300 relative inline-flex h-12 overflow-hidden rounded-full p-[1.5px] focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50'">
+        <span className="absolute inset-[-1000%] animate-[spin_4s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#ffc6c6_0%,#ff0505_50%,#ff9999_100%)]" />
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-gray-950 px-8 py-1 text-sm font-medium text-gray-50 backdrop-blur-3xl">
+          Confirm
+        </span>
+      </div>
     </Link>
   );
 };
--- src/app/components/sections/hero/H1hero.tsx
+++ src/app/components/sections/hero/H1hero.tsx
@@ -1,5 +1,9 @@
 const H1hero = () => {
-  return <h1 className="text-center text-white text-3xl md:text-4xl lg:text-6xl">UNIQUE EXPERIENCE</h1>;
+  return (
+    <h1 className="text-center text-3xl md:text-4xl lg:text-6xl text-white text-shadow-lg shadow-gray-400">
+      UNIQUE EXPERIENCE
+    </h1>
+  );
 };
 
 export default H1hero;
