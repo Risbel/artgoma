dc25658f1f2bf77851593ceaee25a33ff2279a84
Risbel Suarez <risbel961019@gmail.com>
2024-05-21 21:22:18 -0400

    fix: Fix `verifyCollaboratorCode` util

--- src/utils/verifyCollaboratorCode.tsx
+++ src/utils/verifyCollaboratorCode.tsx
@@ -1,7 +1,16 @@
 export const verifyCollaboratorCode = (cadena: string) => {
   const regex = /^[a-zA-Z0-9]{5}$/;
 
-  cadena = cadena.substring(cadena.length - 5);
+  cadena = cadena.substring(cadena.length - 6);
+
+  if (cadena.slice(0, 1) === "/") {
+    cadena = cadena.replace("/", "");
+  } else {
+    return {
+      isCollaborator: false,
+      collaborator: null,
+    };
+  }
 
   if (regex.test(cadena)) {
     return {
